8a049e63ab4287c10db3152af2e5c32393791e21
csmaty <matyas.csiki@gmail.com>
2016-03-17 10:29:13 +0100

    mockable fs version

--- function.js
+++ function.js
@@ -2,17 +2,31 @@
 
 var fs = require('fs');
 
-function delayedFileWriter() {
 
-  this.writeToFile = function(fileToWrite, object, cb) {
-    setTimeout(function(){
+function DelayedFileWriter(alternativeFS) {
+  var _this = this;
+
+  this.fs = alternativeFS || fs;
+
+  this.write = function(fileToWrite, object, cb) {
+    setTimeout(function() {
       var stringifiedObject = JSON.stringify(object);
-      fs.writeFile(fileToWrite, function(err, object) {
+      _this.fs.writeFile(fileToWrite, function(err, stringifiedObject) {
         if (err) {
           return cb(err);
         }
         cb(null, object);
-  });
+      });
     }, 2000);
   };
 }
+
+
+
+var sampleObject = { mennyi: 'mi_mennyi' };
+var file = './file-to-write.txt';
+function logFunction(err, string) {
+  console.log(string);
+}
+var delayedFileWriter = new DelayedFileWriter();
+delayedFileWriter.write(file, sampleObject, logFunction);
